2b389ebbe4bbe30d2c7ac7778ebcb9c5580d2b3c
Gaurav <gaurav.rpandit1@gmail.com>
2023-10-22 22:53:53 +0530

    Deployment to vercel

--- src/utils/APIRoute.js
+++ src/utils/APIRoute.js
@@ -1,4 +1,4 @@
-export const host = "https://smart-room-app.herokuapp.com";
+export const host = "https://smartroomserver.vercel.app";
 export const registrationRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const profileRoute = `${host}/api/auth/setProfile`;
